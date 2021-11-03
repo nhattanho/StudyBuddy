ccb796904c01973a00b91715f8c7b3035e3036c0
Daisy <penguinsliding4@gmail.com>
2021-11-03 03:06:31 -0700

    hardcoded buddies, request details modal

--- frontend/studybuddy/src/App.js
+++ frontend/studybuddy/src/App.js
@@ -12,10 +12,12 @@ import {Provider} from "react-redux"
 
 /* Adding pages/components */
 import Landing from "./pages/landing/landing.js";
+<<<<<<< HEAD
 import Register from "./pages/register/register.js";
 import Home from "./pages/home/home.js";
 import Header from "../src/components/header/header.js";
 import SendingRequest from "./pages/sendingRequest/sendingRequest.js";
+import Buddies from "./pages/buddies/buddies.js";
 
 import './App.css';
 
@@ -28,6 +30,7 @@ function App() {
         <Route exact path="/register" component={Register} />
         <Route exact path="/home" component={Home} />
         <Route exact path="/sendingRequest" component={SendingRequest} />
+        <Route exact path="/buddies" component={Buddies} />
       </div>
     </Provider>
   );
---
+++ frontend/studybuddy/src/pages/buddies/buddies.js
@@ -0,0 +1,270 @@
+import React, { useState } from "react"
+import { makeStyles } from "@material-ui/core/styles";
+import { List, ListItem, Button, Dialog, Card, CardContent, CardMedia } from "@material-ui/core";
+import DannyPic from "./images/danny.jpg";
+import SamPic from "./images/sam.jpg";
+import TuckerPic from "./images/tucker.png";
+import JazzPic from  "./images/jazz.png";
+import ValPic from  "./images/val.png";
+import DashPic from  "./images/dash.png";
+
+const useStyles = makeStyles((theme) => ({
+  threeCol: {
+    display: "flex",
+		justifyContent: "space-between"
+  },
+  column: {
+    width: "100%",
+    height: "100vh",
+    border: "2px solid black",
+  },
+  buddy: {
+  	borderRadius: "10px",
+  	background: "silver",
+  	width: "90%",
+  	height: "6em",
+  	margin: "1em auto",
+  	display: 'flex',
+    alignItems: 'center',
+    justifyContent: "space-between",
+  },
+  buddyImg: {
+  	borderRadius:"50%",
+  	width: "5.5em",
+  	height: "5.5em",
+  	minWidth: "5.5em",
+  	overflow: "hidden",
+  	margin: "1em",
+  },
+	requestDetails: {
+		display: "flex",
+		justifyContent: "space-between",
+		padding: "1em 2em"
+	},
+	options: {
+		display: "flex",
+		flexDirection: "column",
+		paddingTop: "8px"
+	},
+	optionButton: {
+		margin: ".25em 0"
+	}
+}));
+
+const BuddyType = {
+	OUTGOING: "outgoing",
+	INCOMING: "incoming",
+	MATCHED: "matched",
+}
+
+// Remove these hardcodes
+const OUTGOING_BUDDIES = [
+	{
+		id: 1,
+		name: "Valerie Gray",
+		pic: ValPic,
+	},
+	{
+		id: 2,
+		name: "Dash Baxter",
+		pic: DashPic,
+	},
+]
+
+const INCOMING_BUDDIES = [
+	{
+		id: 3,
+		name: "Jazz Fenton",
+		pic: JazzPic,
+	},
+]
+
+const MATCHED_BUDDIES = [
+	{
+		id: 4,
+		name: "Danny Fenton",
+		pic: DannyPic,
+	},
+	{
+		id: 5,
+		name: "Sam Manson",
+		pic: SamPic,
+	},
+	{
+		id: 6,
+		name: "Tucker Foley",
+		pic: TuckerPic,
+	},
+]
+
+const REQUEST_TIMES = [
+	{
+		start: "November 12th 8:00pm",
+		end: "November 12th 11:30pm"
+	},
+	{
+		start: "November 14th 2:30pm",
+		end: "November 14th 4:00pm"
+	}
+]
+
+export default function Buddies() {
+	const classes = useStyles();
+	const [outgoing, setOutgoing] = useState(OUTGOING_BUDDIES);
+	const [incoming, setIncoming] = useState(INCOMING_BUDDIES);
+	const [matched, setMatched] = useState(MATCHED_BUDDIES);
+
+	// TODO: get the actual buddies/requests
+
+	const removeById = (id, buddyList) => {
+		return buddyList.filter(buddy => buddy.id !== id);
+	}
+
+	const handleAcceptRequest = (buddy, handleClose) => {
+		setIncoming(removeById(buddy.id, incoming));
+		setMatched([buddy].concat(matched));
+		handleClose();
+		console.log(incoming)
+
+	}
+
+	const handleRejectRequest = (buddy, handleClose) => {
+		setIncoming(removeById(buddy.id, incoming));
+		handleClose();
+		console.log(incoming)
+	}
+
+	const handleDeleteBuddy = (buddy) => {
+		console.log(matched);
+		setMatched(removeById(buddy.id, matched));
+		console.log(matched)
+	}
+
+	return (
+		<div className={classes.threeCol}>
+			<BuddyColumn type={BuddyType.OUTGOING} buddies={outgoing} />
+			<BuddyColumn type={BuddyType.INCOMING} buddies={incoming} handleAccept={handleAcceptRequest} handleReject={handleRejectRequest}/>
+			<BuddyColumn type={BuddyType.MATCHED} buddies={matched} handleDelete={handleDeleteBuddy}/>
+		</div>
+	);
+}
+
+const BuddyColumn = (props) => {
+	const classes = useStyles();
+	const { type, buddies, ...handleFns } = props;
+
+	let colName;
+	switch (type) {
+		case BuddyType.OUTGOING:
+			colName = "Outgoing Requests";
+			break;
+		case BuddyType.INCOMING:
+			colName = "Incoming Requests";
+			break;
+		case BuddyType.MATCHED:
+			colName = "Matched Buddies";
+			break;
+	}
+
+	let buddyComponents = []
+	for (let i=0; i<buddies.length; i++) {
+		buddyComponents.push(<Buddy key={buddies[i].id} buddyInfo={buddies[i]} type={type} {...handleFns}/>)
+	}
+
+	return (
+		<div className={classes.column}>
+			<h1>{colName}</h1>
+			{buddyComponents}
+		</div>
+	)
+}
+
+const Buddy = (props) => {
+	const classes = useStyles();
+	const [showRequest, setShowRequest] = useState(false);
+	let options, requestDetails;
+
+	switch (props.type) {
+		case BuddyType.OUTGOING:
+			options = null
+			requestDetails = null
+			break;
+		case BuddyType.INCOMING:
+			options =<div className={classes.options}><Button onClick={() => setShowRequest(true)} color="primary" variant="contained">View Request</Button></div>
+			requestDetails =
+				<RequestDetails
+					show={showRequest}
+					handleClose={() => setShowRequest(false)}
+					handleAccept={props.handleAccept}
+					handleReject={props.handleReject}
+					times={REQUEST_TIMES}
+					buddyInfo={props.buddyInfo}>
+				</RequestDetails>
+			break;
+		case BuddyType.MATCHED:
+			options =
+				<div className={classes.options}>
+					<Button color="primary" variant="contained" className={classes.optionButton}>Request</Button>
+					<Button color="secondary" variant="contained" className={classes.optionButton} onClick={() => props.handleDelete(props.buddyInfo)}>Delete</Button>
+				</div>
+			requestDetails = null
+			break;
+	}
+	return (
+		<Card className={classes.buddy}>
+			<CardMedia component="img" image={props.buddyInfo.pic} className={classes.buddyImg}></CardMedia>
+			<CardContent>
+			 	<h2>{props.buddyInfo.name}</h2>
+			</CardContent>
+			<CardContent>
+			 	{options}
+			 	{requestDetails}
+			</CardContent>
+		</Card>
+	)
+}
+
+const RequestDetails = (props) => {
+	const classes = useStyles();
+	return (
+		<Dialog onClose={props.handleClose} open={props.show} maxWidth="sm" fullWidth>
+			<div className={classes.requestDetails}>
+				<RequestedTimes times={props.times}></RequestedTimes>
+				<div className={classes.options}>
+					<Button variant="contained" color="primary" className={classes.optionButton} onClick={() => props.handleAccept(props.buddyInfo, props.handleClose)}>
+						Accept
+					</Button>
+					<Button variant="contained" color="secondary" className={classes.optionButton} onClick={() => props.handleReject(props.buddyInfo, props.handleClose)}>
+						Reject
+					</Button>
+					<Button variant="contained" className={classes.optionButton}>
+						Propose New
+					</Button>
+				</div>
+			</div>
+			<Button variant="outlined" onClick={props.handleClose}>
+				Close
+			</Button>
+		</Dialog>
+	)
+}
+
+const RequestedTimes = (props) => {
+	const [selectedItem, setSelectedItem] = useState(-1);
+
+	let timeListItems = []
+	for (let i=0; i<props.times.length; i++) {
+		timeListItems.push(
+			<ListItem button selected={selectedItem == i} onClick={() => setSelectedItem(i)} key={i}>
+				{props.times[i].start} - {props.times[i].end}
+			</ListItem>
+		)
+	}
+	return (
+		<div>
+			<List>
+				{timeListItems}
+			</List>
+		</div>
+	)
+}
