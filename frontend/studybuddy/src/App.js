471738ba46a95f896e6f61f344362698c9dbca34
cliffdawg <cliffordyin@gmail.com>
2021-11-05 14:42:41 -0700

    Basic profile page frontend

--- frontend/studybuddy/src/App.js
+++ frontend/studybuddy/src/App.js
@@ -8,6 +8,7 @@ import {Provider} from "react-redux"
 /* Adding pages/components */
 import Landing from "./pages/landing/landing.js";
 import Header from "../src/components/header/header.js";
+import Profile from "./pages/profile/profile.js";
 
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
       <div className="App">
         <Header />
         <Route exact path="/" component={Landing} />
+        <Route exact path="/profile" component={Profile} />
       </div>
     </Provider>
   );
