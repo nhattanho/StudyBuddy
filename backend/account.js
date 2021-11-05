c053fb95998c38aa2238ca7cbc01828d42f74b04
Ty Koslowski <tykoslowski@ucla.edu>
2021-11-05 12:06:11 -0700

    Update to rough draft of Accounts API

--- backend/account.js
+++ backend/account.js
@@ -2,18 +2,28 @@
 // By: Ty Koslowski
 
 class Account {
+    #email;
+    #password;
     #name;
     #major;
     #classes;
     #profileURL;
     
-    constructor(name, major) {
-        if (typeof name == 'string') {
-            this.#name = name;
+    constructor(email, password) {
+        if (typeof email == 'string') {
+            this.#email = email;
         } // else throw error ?
-        if (typeof major == 'string') {
-            this.#major = major;
+        if (typeof password == 'string') {
+            this.#password = password;
         } // else throw error ?
+    }
+
+    getEmail() {
+        return this.#email;
+    }
+
+    getPassword() {
+        return this.#password;
     }
 
     getName() {
@@ -30,6 +40,30 @@ class Account {
 
     getProfileURL() {
         return this.#profileURL;
+    }
+
+    changeEmail(new_email) {
+        if (typeof new_email == 'string') {
+            this.#email = new_email;
+        }
+    }
+
+    changePassword(new_password) {
+        if (typeof new_password == 'string') {
+            this.#password = new_password;
+        }
+    }
+
+    setName(name) {
+        if (typeof name == 'string') {
+            this.#name = name;
+        }
+    }
+
+    setMajor(major) {
+        if (typeof major == 'string') {
+            this.#major = major;
+        }
     }
 
     setClasses(classes) {
