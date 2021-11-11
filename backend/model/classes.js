0f25e253dddccf441d33c78cecf9aeafa383baa2
vishnu <devarakonda.vishnu5@gmail.com>
2021-11-11 14:47:43 -0800

    Added classes and class schema

---
+++ backend/model/classes.js
@@ -0,0 +1,342 @@
+/**
+ * File contains list of classes offered in the CS department.
+ * @author Vishnu Devarakonda
+ */
+
+const CS30 = {
+    id: "CS30",
+    name: "Principles and Practices of Computing"
+}
+
+const CS31 = {
+    id: "CS31",
+    name: "Introduction to Computer Science I"
+}
+
+const CS32 = {
+    id: "CS32",
+    name: "Introduction to Computer Science II"
+}
+
+const CS33 = {
+    id: "CS33",
+    name: "Introduction to Computer Organization"
+}
+
+const CS35L = {
+    id: "CS35L",
+    name: "Software Construction"
+}
+
+const CSM51A = {
+    id: "CSM51A",
+    name: "Logic Design of Digital Systems"
+}
+
+const CS97 = {
+    id: "CS97",
+    name: "Variable Topics in Computer Science"
+}
+
+const CS99 = {
+    id: "CS99",
+    name: "Student Research Program"
+}
+
+const CS111 = {
+    id: "CS111",
+    name: "Operating Systems Principles"
+}
+
+const CS112 = {
+    id: "CS112",
+    name: "Modeling Uncertainty in Information Systems"
+}
+
+const CS117 = {
+    id: "CS117",
+    name: "Computer Networks: Physical Layer"
+}
+
+const CS118 = {
+    id: "CS118",
+    name: "Computer Network Fundamentals"
+}
+
+const CSM119 = {
+    id: "CSM119",
+    name: "Fundamentals of Embedded Networked Systems"
+}
+
+const CSCM121 = {
+    id: "CSCM121",
+    name: "Introduction to Bioinformatics"
+}
+
+const CSCM122 = {
+    id: "CSCM122",
+    name: "Algorithms in Bioinformatics"
+}
+
+const CSCM124 = {
+    id: "CSCM124",
+    name: "Machine Learning Applications in Genetics"
+}
+
+const CS130 = {
+    id: "CS130",
+    name: "Software Engineering"
+}
+
+const CS131 = {
+    id: "CS131",
+    name: "Programming Languages"
+}
+
+const CS132 = {
+    id: "CS132",
+    name: "Compiler Construction"
+}
+
+
+const CS133 = {
+    id: "CS133",
+    name: "Parallel and Distributed Computing"
+}
+
+const CS134 = {
+    id: "CS134",
+    name: "Distributed Systems"
+}
+
+const CS136 = {
+    id: "CS136",
+    name: "Introduction to Computer Security"
+}
+
+const CSC137A = {
+    id: "CSC137A",
+    name: "Prototyping Programming Languages"
+}
+
+const CSC137B = {
+    id: "CSC137B",
+    name: "Programming Language Design"
+}
+
+const CS143 = {
+    id: "CS143",
+    name: "Data Management Systems"
+}
+
+const CS144 = {
+    id: "CS144",
+    name: "Web Applications"
+}
+
+const CS145 = {
+    id: "CS145",
+    name: "Introduction to Data Mining"
+}
+
+const M146 = {
+    id: "M146",
+    name: "Introduction to Machine Learning"
+}
+
+const M148 = {
+    id: "M148",
+    name: "Introduction to Data Science"
+}
+
+const M151B = {
+    id: "M151B",
+    name: "Computer Systems Architecture"
+}
+
+const M152A = {
+    id: "M152A",
+    name: "Introductory Digital Design Laboratory"
+}
+
+const CS152B = {
+    id: "CS152B",
+    name: "Digital Design Project Laboratory"
+}
+
+const CS161 = {
+    id: "CS161",
+    name: "Fundamentals of Artificial Intelligence"
+}
+
+const CS168 = {
+    id: "CS168",
+    name: "Computational Methods for Medical Imaging"
+}
+
+
+const CS170A = {
+    id: "CS170A",
+    name: "Mathematical Modeling and Methods for Computer Science"
+}
+
+const M171L = {
+    id: "M171L",
+    name: "Data Communication Systems Laboratory"
+}
+
+const CS172 = {
+    id: "CS172",
+    name: "Real-Time Three-Dimensional Animation"
+}
+
+const CS174A = {
+    id: "CS174A",
+    name: "Introduction to Computer Graphics"
+}
+
+const CS174B = {
+    id: "CS174B",
+    name: "Introduction to Computer Graphics: Three-Dimensional Photography and Rendering"
+}
+
+const C174C = {
+    id: "C174C",
+    name: "Computer Animation"
+}
+
+const CS180 = {
+    id: "CS180",
+    name: "Introduction to Algorithms and Complexity"
+}
+
+const CS181 = {
+    id: "CS181",
+    name: "Introduction to Formal Languages and Automata Theory"
+}
+
+const CSM182 = {
+    id: "CSM182",
+    name: "Dynamic Biosystem Modeling and Simulation Methodology"
+}
+
+const CS183 = {
+    id: "CS183",
+    name: "Introduction to Cryptography"
+}
+
+const CSM184 = {
+    id: "CSM184",
+    name: "Introduction to Computational and Systems Biology"
+}
+
+const CM186 = {
+    id: "CM186",
+    name: "Computational Systems Biology: Modeling and Simulation of Biological Systems"
+}
+
+const CM187 = {
+    id: "CM187",
+    name: "Research Communication in Computational and Systems Biology"
+}
+
+const CS188 = {
+    id: "CS188",
+    name: "Special Courses in Computer Science"
+}
+
+const CS188SA = {
+    id: "CS188SA",
+    name: "Individual Studies for USIE Facilitators"
+}
+
+const CS188SB = {
+    id: "CS188SB",
+    name: "Individual Studies for USIE Facilitators"
+}
+
+const CS188SC = {
+    id: "CS188SC",
+    name: "Individual Studies for USIE Facilitators"
+}
+
+const CS192 = {
+    id: "CS192",
+    name: "Methods and Application of Collaborative Learning Theory in Life Sciences"
+}
+
+const CSM192A = {
+    id: "CSM192A",
+    name: "Introduction to Collaborative Learning Theory and Practice"
+}
+
+const CS194 = {
+    id: "CS194",
+    name: "Research Group Seminars: Computer Science"
+}
+
+const CS199 = {
+    id: "CS199",
+    name: "Directed Research in Computer Science"
+}
+
+
+const classes = [
+    CS30,
+    CS31,
+    CS32,
+    CS33,
+    CS35L,
+    CSM51A,
+    CS97,
+    CS99,
+    CS111,
+    CS112,
+    CS117,
+    CS118,
+    CSM119,
+    CSCM121,
+    CSCM122,
+    CSCM124,
+    CS130,
+    CS131,
+    CS132,
+    CS133,
+    CS134,
+    CS136,
+    CSC137A,
+    CSC137B,
+    CS143,
+    CS144,
+    CS145,
+    M146,
+    M148,
+    M151B,
+    M152A,
+    CS152B,
+    CS161,
+    CS168,
+    CS170A,
+    M171L,
+    CS172,
+    CS174A,
+    CS174B,
+    C174C,
+    CS180,
+    CS181,
+    CSM182,
+    CS183,
+    CSM184,
+    CM186,
+    CM187,
+    CS188,
+    CS188SA,
+    CS188SB,
+    CS188SC,
+    CS192,
+    CSM192A,
+    CS194,
+    CS199
+]
+
+module.exports = classes
