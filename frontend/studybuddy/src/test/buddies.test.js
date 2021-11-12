34c9dfc1dd615dff611dcc6864e607f47dfc2077
Daisy <penguinsliding4@gmail.com>
2021-11-11 17:55:23 -0800

    basic rendering tests for buddies page

---
+++ frontend/studybuddy/src/test/buddies.test.js
@@ -0,0 +1,43 @@
+import { unmountComponentAtNode } from "react-dom";
+import { render, screen, within } from '@testing-library/react'
+import Buddies from "../pages/buddies/buddies.js";
+
+// var rewire = require('rewire');
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe("Tests component rendering", () => {
+	// TODO: mock the api calls to return test data
+	// For now just render and test on the hardcoded data since implementation will change in the future
+	
+	test("Test all 3 columns rendered", async() => {	
+		render(<Buddies />);
+		expect(screen.getByText('Outgoing Requests')).toBeInTheDocument();
+		expect(screen.getByText('Incoming Requests')).toBeInTheDocument();
+		expect(screen.getByText('Matched Buddies')).toBeInTheDocument();
+	});
+
+	test("Test incoming request has 'View Request' button", async() => {	
+		render(<Buddies />);
+		expect(screen.getByText('Jazz Fenton').closest('div.MuiCard-root')).toContainElement(screen.getByText('View Request'));
+	});
+
+	test("Test matched buddy has 'Request' and 'Delete' buttons", async() => {	
+		render(<Buddies />);
+		let buddyCard = screen.getByText('Danny Fenton').closest('div.MuiCard-root');
+		expect(buddyCard).toContainElement(within(buddyCard).getByText('Request'));
+		expect(buddyCard).toContainElement(within(buddyCard).getByText('Delete'));
+	});
+});
